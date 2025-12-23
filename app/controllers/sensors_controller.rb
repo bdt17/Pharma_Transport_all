cdaa9b39e91422b22cea4fb93121b71b598127a6
bdt17 <brett.thomas29.97@gmail.com>
2025-12-23 14:28:00 -0700

    FIX: Clean sensors_controller.rb syntax - Phase 10+ APIs

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -2,19 +2,6 @@ class SensorsController < ApplicationController
   def index
     render json: [{id:1,truck_id:1,temperature:4.2,"PHARMA":"TRUCK SENSORS 🚚 FDA"}]
   end
-end
-
-
-def forecast
-  result = SensorReading.forecast_demand(params[:vehicle_id])
-  render json: { forecast: result, status: 'success' }
-end
-
-def tamper
-  vehicle = Vehicle.find(params[:vehicle_id])
-  result = vehicle.detect_tamper(params[:vibration].to_f, params[:light].to_f)
-  render json: result, status: :ok
-
 
   def forecast
     result = SensorReading.forecast_demand(params[:vehicle_id])
@@ -26,6 +13,4 @@ def tamper
     result = vehicle.detect_tamper(params[:vibration].to_f, params[:light].to_f)
     render json: result, status: :ok
   end
-
-
 end
