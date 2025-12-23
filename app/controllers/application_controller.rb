0198c26883393a2fc7e6644ff5213c774dcd0506
bdt17 <brett.thomas29.97@gmail.com>
2025-12-22 18:43:31 -0700

    CORS mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
+
+protect_from_forgery with: :null_session, if: -> { request.format.json? }
+after_action :cors_set_access_control_headers
+
+def cors_set_access_control_headers
+  headers['Access-Control-Allow-Origin'] = '*'
+  headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+  headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+end
+
+
+
+
 class ApplicationController < ActionController::Base
   # No authentication needed for demos
   # Phase 6-8: Public FDA/Pharma endpoints
